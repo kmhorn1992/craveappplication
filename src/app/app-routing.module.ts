fd6d749dc455460a629a870c40fc9b632ace3f98
sturgilltyffani <sturgilltyffani@gmail.com>
2017-12-13 16:15:11 -0600

    styling added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RestComponent} from './rest/rest.component';
 import { SinglerestComponent } from './singlerest/singlerest.component';
+import { SavedComponent } from './saved/saved.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rest-page', pathMatch: 'full' },
   { path: 'rest-page', component: RestComponent},
   { path: 'single-page', component: SinglerestComponent},
+  { path: 'saved', component: SavedComponent }
  
   
 
