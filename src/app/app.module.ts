fdaac12315f9f537b8af7b707e9aa8f3e2a9d0b9
sturgilltyffani <sturgilltyffani@gmail.com>
2017-12-13 15:45:41 -0600

    ng imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatToolbarModule, MatIconModule, MatTabsModule, MatGridListModule } from '@angular/material'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { Http, HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
 import { RestComponent } from './rest/rest.component';
 import { SinglerestComponent } from './singlerest/singlerest.component';
+import { SavedComponent } from './saved/saved.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RestComponent,
-    SinglerestComponent
+    SinglerestComponent,
+    SavedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
